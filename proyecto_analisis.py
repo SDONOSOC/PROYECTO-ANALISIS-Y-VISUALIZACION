393a1a02d221958b9b0df7a1d084aadfe57405da
SDONOSO <sdonoso@gmail.com>
2023-06-30 14:52:01 -0400

    CAMBIO NOMBRE ARCHIVO

--- proyecto_analisis.py
+++ proyecto_analisis.py
@@ -1,5 +1,6 @@
-#importar librerias generales
+#importar librerias general
 import pandas as pd
 
 #cargar dataframe
-detalles_por_articulo=pd.read_csv("OC DETALLADAS POR ARTCULOS AL 31052023.csv")
+detalles_por_articulo = pd.read_csv('c:\Users\sdono\Documents\CURSO VISUALIZACIÓN DE DATOS\PROYECTO\PROYECTO-ANALISIS-Y-VISUALIZACION\OC DETALLADAS POR ARTCULOS AL 31052023', encoding='utf-8')
+
