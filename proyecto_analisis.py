c95c1372863c7ddce86b2fd7fdb66555aa79e749
SDONOSO <sdonoso@gmail.com>
2023-06-30 17:53:04 -0400

    a

--- proyecto_analisis.py
+++ proyecto_analisis.py
@@ -2,5 +2,5 @@
 import pandas as pd
 
 #cargar dataframe
-detalles_por_articulo = pd.read_csv('c:\Users\sdono\Documents\CURSO VISUALIZACIÓN DE DATOS\PROYECTO\PROYECTO-ANALISIS-Y-VISUALIZACION\OC DETALLADAS POR ARTCULOS AL 31052023', encoding='utf-8')
-
+detalles_por_articulo=pd.read_csv('/OC DETALLADAS POR ARTCULOS AL 31052023')
+ 
