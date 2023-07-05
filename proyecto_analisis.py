1512fc71d05e947f960e0cca77c7c006a7d7ee65
SDONOSO <sdonoso@gmail.com>
2023-07-04 20:34:08 -0400

    PRUEBA

--- proyecto_analisis.py
+++ proyecto_analisis.py
@@ -12,3 +12,5 @@ import pandas as pd
 df_detalles_por_ejecutivo= pd.read_csv(r'./OC_DETALLADAS_POR_EJECUTIVO_Y_OC_AL_31052023.csv',encoding='utf-8',sep = ';')
 #mostrar los primeros 5 resultados
 print(df_detalles_por_ejecutivo.head(5))
+
+print("hola mundo")
